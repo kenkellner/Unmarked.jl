fc9c2f364e86409f2ac079d5a31d99291e97fd2d
Ken Kellner <ken@kenkellner.com>
2019-07-25 20:21:54 -0400

    Add Royle-Nichols occupancy model

--- occ_rn.jl
+++ occ_rn.jl
@@ -6,40 +6,40 @@ using LinearAlgebra
 Random.seed!(123);
 
 #Simulate data
-N = 100;
+N = 400;
 J = 5;
-z = rand(Poisson(10),N);
-p = 0.4;
+z = rand(Poisson(5),N);
+r = 0.1;
 
 y = Array{Int64,2}(undef, N, J);
 
 for i = 1:N
   for j = 1:J
-    y[i,j] = rand(Binomial(z[i], p));
+    p = 1 - (1-r)^z[i];
+    y[i,j] = rand(Binomial(1, p));
   end
 end
 
 #Population size values to marginalize over
-K = maximum(y) + 20
+K = 25
 
 #Estimate
 
 function loglik(β_raw::Array)
   
   λ = exp(β_raw[1])
-  p = logistic(β_raw[2])
+  r = logistic(β_raw[2])
    
   ll = zeros(eltype(β_raw),N)
   for i = 1:N
     fg = zeros(eltype(β_raw),K+1)
     for k = 0:K
-      if maximum(y[i,]) > k
-        continue
-      end
       fk = pdf(Poisson(λ), k)
       gk = 1.0
       for j = 1:J
-        gk *= pdf(Binomial(k, p), y[i,j])
+        #repeatedly calculating to allow covs later
+        p = 1 - (1 - r)^k 
+        gk *= p^y[i,j] * (1-p)^(1-y[i,j])
       end
       fg[k+1] = fk * gk
     end
@@ -58,7 +58,7 @@ vcov = inv(hes);
 se = sqrt.(diag(vcov));
 
 #Display
-truth = [log(10), logit(p)]; 
+truth = [log(5), logit(r)]; 
 pnames = ["β[1]", "β[2]"];
 cnames = ["param" "truth" "estimate" "se"];
 [cnames; pnames truth param se]
